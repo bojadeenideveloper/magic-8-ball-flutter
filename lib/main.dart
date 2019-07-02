8e3ffdec59ddd40870cd33f7eee1a06e9dadf9ff
jwtruver <developer@bojadeeni.com>
2019-07-02 15:30:33 -0400

    07-02 03-30 Changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+//void main() => runApp(
+//      MaterialApp(
+//        home: null,
+//      ),
+//    );
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+//        body: DicePage(),
       ),
-    );
+    ),
+  );
+}
